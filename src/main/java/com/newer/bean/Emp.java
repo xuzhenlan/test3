65b8f194c42583259545c2e67b0876df7610187f
xuzhenlan <982310462@qq.com>
2018-01-05 20:21:01 +0800

    Emp类

--- src/main/java/com/newer/bean/Emp.java
+++ src/main/java/com/newer/bean/Emp.java
@@ -1,4 +1,13 @@
 package com.newer.bean;
 
-public class Emp {
+public class Emp{
+    private String ename;
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
 }
